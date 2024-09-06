076dea8ae19ab2ff4487360a6474503b2424fbb4
TAIJULAMAN <taijulaman8@gmail.com>
2024-09-06 06:18:53 +0600

    nav and hero changed

--- src/Components/Doc/Doc.jsx
+++ src/Components/Doc/Doc.jsx
@@ -1,7 +1,13 @@
 import navImage from '/public/nav.jpg'
+import nav1 from '../../../public/nav1.jpg'
+import nav2 from '../../../public/nav2.jpg'
+import nav3 from '../../../public/nav3.jpg'
+import nav4 from '../../../public/nav4.jpg'
+import nav5 from '../../../public/nav5.jpg'
+import nav6 from '../../../public/nav6.jpg'
 
 const Doc = () => {
-    return {navImage}
+    return {navImage, nav1, nav2, nav3, nav4, nav5, nav6}
   }
 
 export default Doc
--- src/Components/Home/VideoSlider.jsx
+++ src/Components/Home/VideoSlider.jsx
@@ -1,31 +1,22 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const slides = [
   {
-    videoSrc: '/videos/vid8.2.mp4',
-    title: 'EXPAND YOUR JOURNEY',
-    description: 'Where to go? What to do? Get started, explore the life you want to live.',
+    videoSrc: '/videos/vid9.mp4',
+    title: 'Discover the Magic of Japan',
+    description: 'Japan, a land where ancient temples coexist with neon-lit skyscrapers, offers an unparalleled travel experience. From the tranquil gardens of Kyoto to the bustling streets of Tokyo, this island nation enchants visitors with its perfect balance of tradition and modernity. Whether you’re a foodie, history buff, or adventure-seeker, Japan promises an unforgettable journey',
   },
   {
-    videoSrc: '/videos/vid2.mp4',
-    title: 'TRAVEL AROUND JAPAN',
-    description: 'Endless Discovery: Adventure and Discovery is worthwhile and endless, travel far enough, seek for yourself, seek other places, seek other lives, other souls, and culture.',
-  },
-  {
-    videoSrc: '/videos/vid1.mp4',
-    title: 'EXPERIENCE TOKYO',
-    description: 'Tokyo Tokyo Old meets New: Tokyo has it all, leading the way in technological advancements while upholding its deeply ingrained traditions.',
+    videoSrc: '/videos/vid3.mp4',
+    title: 'Discover the Magic of Japan',
+    description: 'Japan, a land where ancient temples coexist with neon-lit skyscrapers, offers an unparalleled travel experience. From the tranquil gardens of Kyoto to the bustling streets of Tokyo, this island nation enchants visitors with its perfect balance of tradition and modernity. Whether you’re a foodie, history buff, or adventure-seeker, Japan promises an unforgettable journey',
   },
   {
     videoSrc: '/videos/vid4.mp4',
-    title: 'VISIT OSAKA',
-    description: 'Venice of the East: Blends ancient history with ultra-modern attractions, Japan\'s third largest and most fun-loving city.',
-  },
-  {
-    videoSrc: '/videos/vid5.mp4',
-    title: 'DISCOVER KYOTO',
-    description: 'Visit Kyoto to experience its wonders and to enrich your travels with its history, culture, and untouched nature.',
-  },
+    title: 'Discover the Magic of Japan',
+    description: 'Japan, a land where ancient temples coexist with neon-lit skyscrapers, offers an unparalleled travel experience. From the tranquil gardens of Kyoto to the bustling streets of Tokyo, this island nation enchants visitors with its perfect balance of tradition and modernity. Whether you’re a foodie, history buff, or adventure-seeker, Japan promises an unforgettable journey',
+  }
 ];
 
 const VideoSlider = () => {
@@ -34,47 +25,52 @@ const VideoSlider = () => {
   useEffect(() => {
     const slideInterval = setInterval(() => {
       setCurrentSlide((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
-    }, 5000); // Change slide every 5 seconds
+    }, 6000);
 
-    return () => clearInterval(slideInterval); // Cleanup interval on unmount
+    return () => clearInterval(slideInterval);
   }, []);
 
   return (
-    <section id="home" className=" relative  bg-black h-[700px]">
-    {slides.map((slide, index) => (
-      <div
-        key={index}
-        className={`absolute top-0 left-0 w-full h-full transition-opacity duration-500 ease-in-out ${
-          index === currentSlide ? 'opacity-100 z-10' : 'opacity-0 z-0'
-        }`}
-      >
-        <video
-          src={slide.videoSrc}
-          loop
-          autoPlay
-          muted
-          className="object-cover w-full h-full opacity-60"
-        ></video>
-        <div className="caption absolute 
-        left-1/4 md:left-1/2 top-[40%] transform -translate-x-1/2 -translate-y-1/2 w-[60%] text-center  max-sm:ml-28 sm:ml-8  max-sm:-mt-10 sm:-mt-4">
-          <h1 className="text-white font-extrabold text-[clamp(4rem,7vw,7rem)] leading-tight
-          max-sm:text-2xl sm:text-2xl md:text-5xl lg:text-5xl max-xl:text-7xl  
-          ">
-            {slide.title}
-          </h1>
-          <p className="text-white mt-4">{slide.description}</p>
+    <section id="home" className="relative bg-black h-[500px] md:h-[650px]">
+      {slides.map((slide, index) => (
+        <div
+          key={index}
+          className={`absolute top-0 left-0 w-full h-full transition-opacity duration-600 ease-in-out ${index === currentSlide ? 'opacity-100 z-10' : 'opacity-0 z-0'
+            }`}
+        >
+          <video
+            src={slide.videoSrc}
+            loop
+            autoPlay
+            muted
+            className="object-cover w-full h-full opacity-60"
+          ></video>
+          <div className="caption absolute 
+        left-40 md:left-1/2 top-[55%] md:top-[45%] transform -translate-x-1/2 -translate-y-1/2 w-[100%] text-center mx-10">
+            <h1 className="text-white font-bold text-[clamp(4rem,7vw,7rem)] leading-tight
+          text-xl md:text-5xl md:mr-10">
+              {slide.title}
+            </h1>
+            <p className="text-white mt-5 mr-7 md:mr-20 ml-7 md:ml-5">{slide.description}</p>
+            <div className="mt-5 mr-7 md:mr-20 ml-7 md:ml-5">
+              <Link to="/">
+                <button className="text-xl shadow-lg text-[#A04747] font-semibold hover:bg-[#A04747] hover:text-white  bg-white px-5 py-2 rounded-md transition duration-300 ease-in-out">
+                  Get Startted
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
-    ))}
-  
-    {/* Image placed at the bottom */}
-    <img
-      src="/grad2.png"
-      alt="Gradient"
-      className="absolute bottom-0 left-0 w-full z-10 "
-    />
-  </section>
-  
+      ))}
+
+      {/* Image placed at the bottom */}
+      <img
+        src="/grad2.png"
+        alt="Gradient"
+        className="absolute bottom-0 left-0 w-full z-10 "
+      />
+    </section>
+
   );
 };
 
