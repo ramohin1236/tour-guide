1bfe7d2d66479c8b783ad5eafae63788f57ef738
tohirul <tohirul.islam164@gmail.com>
2024-09-12 19:09:15 +0600

    up to date

--- src/Main/MainLayout.jsx
+++ src/Main/MainLayout.jsx
@@ -1,15 +1,25 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "../Sharred/Navbar"
-import Footer from "../Sharred/Footer/Footer"
+import { Outlet } from "react-router-dom";
+import Navbar from "../Sharred/Navbar";
+import Footer from "../Sharred/Footer/Footer";
+import { allUsers } from "../common/api/ApiKit";
+import { useEffect } from "react";
 
 const MainLayout = () => {
+  const getAllUsers = async () => {
+    const users = await allUsers();
+    console.log(users);
+  };
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
   return (
     <div>
-        <Navbar/>
-        <Outlet/>
-        <Footer/>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
