36638de7305c8bb4e449443d00554d7f0062793b
TAIJULAMAN <taijulaman8@gmail.com>
2024-09-14 02:25:29 +0600

    aman

--- src/Components/Home/Travel/TravelCard.jsx
+++ src/Components/Home/Travel/TravelCard.jsx
@@ -4,17 +4,16 @@ import { BsFillCursorFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 const TravelCard = ({ card, calculatePosition, index }) => {
-  
-    return (
+  return (
     <div
       key={card.id}
       className="absolute w-64 h-80 md:w-72 md:h-96 lg:w-80 lg:h-96 rounded-lg shadow-lg transition-transform duration-700"
       style={calculatePosition(index)}
     >
       {/* Discount Badge */}
-      <div className="absolute top-0 right-0 bg-red-600 text-white text-xs font-bold px-2 py-1">
+      {/* <div className="absolute top-0 right-0 bg-red-600 text-white text-xs font-bold px-2 py-1">
         {card.discount}
-      </div>
+      </div> */}
 
       {/* Image */}
       <img
@@ -31,11 +30,13 @@ const TravelCard = ({ card, calculatePosition, index }) => {
       {/* Card Content */}
       <div className="p-4">
         <div className="flex justify-between items-center">
-          <h3 className="text-lg md:text-xl lg:text-2xl font-bold text-[#451717]">{card.title}</h3>
+          <h3 className="text-lg md:text-xl lg:text-2xl font-bold text-[#451717]">
+            {card.title}
+          </h3>
         </div>
         <Link
           to="/"
-          className="text-blue-500 font-semibold text-sm md:text-lg mt-2 flex items-center justify-between"
+          className="text-[#A04747] font-semibold text-sm md:text-lg mt-2 flex items-center justify-between"
         >
           <p>Book Now</p>
           <BsFillCursorFill className="text-2xl animate-moveLeftRight inline-block" />
--- src/Components/Home/Traveller/Traveller.jsx
+++ src/Components/Home/Traveller/Traveller.jsx
@@ -1,32 +1,58 @@
-import Doc from "../../Doc/Doc"
+import Doc from "../../Doc/Doc";
 
 const Traveller = () => {
-    const {naraPark, fuziMountain, omotesando}=Doc()
+  const { naraPark, fuziMountain, omotesando } = Doc();
   return (
     <div className="mx-auto px-[20px] md:px-[100px] mb-10">
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-        <div className="col-span-1 lg:col-span-1" data-aos="fade-right" data-aos-duration="3000">
-          <h2 className="text-xl md:text-5xl font-semibold text-[#A04747]">BEST TRAVELER SHARED PHOTO</h2>
-          <p className="mt-4 text-justify">
-            Excellent opportunity to sample local cuisine and culture while seeing the main attractions.
-            A good mix of modern, traditional, and historical sights or experiences. RYOKOU handled the transportation arrangements for us, making it simple for us to move between cities and nations.
-          </p>
+      <div className="text-center">
+        <h2 className="text-xl md:text-6xl font-bold text-[#A04747]">
+          BEST TRAVELER SHARED PHOTO
+        </h2>
+        <p className="mt-5 text-center px-[20px] md:px-[200px] pb-10">
+          Excellent opportunity to sample local cuisine and culture while seeing
+          the main attractions. A good mix of modern, traditional, and
+          historical sights or experiences. RYOKOU handled the transportation
+          arrangements for us, making it simple for us to move between cities
+          and nations.
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-2 mx-auto px-[20px] md:px-[100px]">
+        <div>
+          <img
+            src={fuziMountain}
+            alt="Travelers in Kyoto"
+            className="rounded shadow-lg h-[250px] w-[400px] md:h-[350px] md:w-[500px]"
+          />
         </div>
-        <div className="col-span-1 lg:col-span-1" data-aos="fade-up" data-aos-duration="3000">
-          <img src={fuziMountain} alt="Travelers in Kyoto" className="rounded shadow-lg h-[300px]" />
+        <div className="">
+          <img
+            src={omotesando}
+            alt="Travelers in Tokyo"
+            className="rounded shadow-lg  h-[250px] w-[400px] md:h-[350px] md:w-[500px]"
+          />
         </div>
-        <div className="col-span-1 lg:col-span-1" data-aos="fade-left" data-aos-duration="3000">
-          <img src={omotesando} alt="Travelers in Tokyo" className="rounded shadow-lg h-[300px]" />
+        <div className="">
+          <img
+            src={naraPark}
+            alt="Travelers in Osaka"
+            className="rounded shadow-lg h-[250px] w-[400px] md:h-[350px] md:w-[500px]"
+          />
         </div>
-        <div className="col-span-1 lg:col-span-1" data-aos="fade-right" data-aos-duration="3000">
-          <img src={naraPark} alt="Travelers in Osaka" className="rounded shadow-lg h-[300px]" />
-        </div>
-        <div className="col-span-1 lg:col-span-2" data-aos="fade-left" data-aos-duration="3000">
-        <iframe  width="100%"  height="300" src="https://www.youtube.com/embed/G5RpJwCJDqc?si=8N1bkCwBnpY5ITEi" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"  allowFullScreen></iframe>
+      </div>
+      <div className="mt-10 mx-auto px-[20px] md:px-[100px] md:pr-[130px]">
+        <div>
+          <iframe
+            width="100%"
+            height="500"
+            src="https://www.youtube.com/embed/G5RpJwCJDqc?si=8N1bkCwBnpY5ITEi"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Traveller
+export default Traveller;
